dc8fd8585a1acb1ee37b224dd04132d6b0dda7c3
Roy <rwang013@uottawa.ca>
2024-03-19 13:20:52 -0400

    ci up

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # LazyGPR
 Lazy-evaluation local Gaussian process regression.
 
-There is an image up-conversion walk-through in `examples/upconvert.jl`.
+## Documentation
+The [documentation](https://royccwang.github.io/LazyGPR.jl/) for examples. Additionally, there is an image up-conversion walk-through in `examples/upconvert.jl`.
 
+## Citation
+You can cite this repository and code via the *Cite This Repository* button on the GitHub webpage of this repository.
--- docs/make.jl
+++ docs/make.jl
@@ -21,28 +21,28 @@ makedocs(
     ],
 )
 
-# # github.
-# makedocs(
-#     sitename="LazyGPR.jl",
-#     modules=[LazyGPR],
-#     format=Documenter.HTML(prettyurls = get(ENV, "CI", nothing)=="true"),
-#     pages = [
-#         "Overview" => "index.md",
-#         "Public API" => "api.md",
+# github.
+makedocs(
+    sitename="LazyGPR.jl",
+    modules=[LazyGPR],
+    format=Documenter.HTML(prettyurls = get(ENV, "CI", nothing)=="true"),
+    pages = [
+        "Overview" => "index.md",
+        "Public API" => "api.md",
         
-#         "Visualize Warp Samples" =>
-#         "generated/compare_warpmaps_lit.md",
+        "Visualize Warp Samples" =>
+        "generated/compare_warpmaps_lit.md",
 
-#         "Visualize Adjustment Map" =>
-#         "generated/s_map_lit.md",
+        "Visualize Adjustment Map" =>
+        "generated/s_map_lit.md",
 
-#         "Demo: Oscillation Reduction" =>
-#         "generated/rainfall_lit.md",
-#     ],
-# )
-# deploydocs(
-#     repo = "github.com/RoyCCWang/LazyGPR.jl",
-#     target = "build",
-#     branch = "gh-pages",
-#     versions = ["stable" => "v^", "v#.#" ],
-# )
+        "Demo: Oscillation Reduction" =>
+        "generated/rainfall_lit.md",
+    ],
+)
+deploydocs(
+    repo = "github.com/RoyCCWang/LazyGPR.jl",
+    target = "build",
+    branch = "gh-pages",
+    versions = ["stable" => "v^", "v#.#" ],
+)
